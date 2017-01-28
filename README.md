ef2ac3f86fae436179ee289986a841f37cf0f6a6
taarush <taarushv15@gmail.com>
2017-01-28 23:21:45 +0530

    Added sign-in/sign-up auth

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Diary
+
+OPENSOURCE ENCRYPTED ONLINE PRIVATE DIARY.
+
+COMING SOON.
+
+COMPLETED:
+1)SIGN UP
+2)SIGN IN AUTH
+
+TO DO:
+1)Adding/viewing content
+2)User profile/follow/feed/settings(change pass etc)/
+3)Peoples Public content
+4)HTML+CSS+JS UI/UX SHIT!
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask, request , render_template , redirect
 import time , sqlite3
-
+global auth
+auth = 0
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/',methods = ['GET','POST'])
 def index():
-    return render_template("root.html")
+    global auth
+    if auth == 1:
+        return redirect('/posts',code=302)
+    else:
+        return render_template("root.html")
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
@@ -18,23 +24,60 @@ def signup():
 
 @app.route('/verify-signup' , methods = ['GET','POST'])
 def verify():
-    global password, username , email , password2
+    global password, username , email , password2, auth
     password2 = str(request.form['password2'])
     password = str(request.form['password'])
     username = str(request.form['username'])
     conn = sqlite3.connect('stuff.db')
     c = conn.cursor()
     if password == password2:
-        c.execute("SELECT * FROM user_details WHERE usernames=?", (username,))
-        ppp = c.fetchall()
-        if len(ppp)==0:
-            c.execute("INSERT INTO user_details")
-
+        c.execute("SELECT * FROM user_details WHERE username=?", (username,))
+        stuff = c.fetchall() #NOTE TO SELF : HAVE TO ADD A LOT OF VERIFICATION TO MAKE SURE PASSWORD LENGTH , CHARECTERS ETC ARE LEGIT, NO SQLITE INJECTIONS
+        if len(stuff)==0:
+            c.execute("insert into user_details ( serial_number , username,password) values (null , ?,?)",
+                      (username, password))
+            conn.commit()
+            auth = 1
+            return redirect('/',code=302)
+        else:
+            return "USERNAME TAKEN. GO BACK AND CHOOSE OTHER ONE SON"
+            #comebacklater
     else:
-        redirect()
+        return "PASSWORDS DON'T MATCH. GO BACK AND TRY AGAIN"
 
-@app.route('/login_verification')
+@app.route('/login_verification',methods = ['GET','POST'])
+def login_verification():
+    global auth,username,password
+    username = str(request.form['login-username'])
+    password = str(request.form['login-password'])
+    conn = sqlite3.connect('stuff.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM user_details WHERE username=?",(username,))
+    stuff = c.fetchall()
+    if len(stuff)==0:
+        return "INVALID CREDENTIALS. GO BACK AND TRY AGAIN"
+    else:
+        for details in stuff:
+            db_username = details[1]
+            db_password = details[2]
+            if db_password == password and db_username == username:
+                auth = 1
+
+                return redirect('/',code=302)
+            else:
+                return "INVALID CREDENTIALS. GO BACK AND TRY AGAIN"
+'''
+
+@app.route('/error')
+
+@app.route('/settings')
+
+@app.route('/<username>/') #COMING SOON!!!!!!!!!!!!!!!!!!!!!!!!
+
+@app.route('/posts')
 
 
+@app.route('/<username>/public')
+'''
 if __name__ == "__main__":
         app.run(debug=True)
